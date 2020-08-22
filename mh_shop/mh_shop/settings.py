d9d915b58c4cc07e96f0094d42a5408cca7364a7
mgbo <mmmmipt307@gmail.com>
2020-08-22 19:00:45 +0630

    add order models

--- mh_shop/mh_shop/settings.py
+++ mh_shop/mh_shop/settings.py
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    
     'shop.apps.ShopConfig',
     'cart.apps.CartConfig',
+    'orders.apps.OrdersConfig',
     'sorl.thumbnail',
 ]
 
@@ -66,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processors.cart',
             ],
         },
     },
