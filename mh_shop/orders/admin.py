e4275bb52d6eced3b0fd6a15bde20e5ab8e613fe
mgbo <mmmmipt307@gmail.com>
2020-08-24 07:16:12 +0630

    create bill

--- mh_shop/orders/admin.py
+++ mh_shop/orders/admin.py
@@ -1,3 +1,13 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Order, OrderItem
+
+class OrderItemInLine(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ['product']
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'phone', 'email', 'city', 'address', 'created', 'updated']
+    list_filter = ['created', 'updated']
+    inlines = [OrderItemInLine]
